8103ca3d7d22d2fe447adf0311ae459e41be3126
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2023-02-10 08:00:45 -0800

    Updated File storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,4 +26,16 @@ class FileStorage:
             d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
 
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+
+
 
