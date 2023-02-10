a5d3d74bf8d5cd03b43582533ba38b397f47a064
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2023-02-10 07:54:07 -0800

    Updated File storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,16 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
 
+    def new(self, obj):
+
+                   """sets in __objects the obj with key <obj class name>.id"""
+
+                           key = "{}.{}".format(type(obj).__name__, obj.id)
+
+                                   FileStorage.__objects[key] = obj
 
 
