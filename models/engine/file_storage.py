0596665ec4e7696f86665cf602394ae82f72dff5
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2023-02-10 07:52:12 -0800

    File storage file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for FileStorage class."""
+import datetime
+import json
+import os
+
+
+class FileStorage:
+
+    """Class for storing and retrieving data"""
+    __file_path = "file.json"
+    __objects = {}
+
+
+
+
